3e52bbc8e9234a11146584bd7683d97757c83455
Alyssa1217 <acapistran@hshm.info>
2019-10-28 22:06:48 +0000

    10_28_19 jQuery

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("button").click(function(){
+    // $("output").text("");
+    var pigLatin = $("input").val();
+    var pigLatin_array = pigLatin.split();
 
 
-
-  
-  
-  
-  
+    console.log(pigLatin_array);
+    $('.output').append();
+});
 });
